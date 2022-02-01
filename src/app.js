8999fc19b96d5b8ce1305ac1a284679531f646ff
techstackmedia <osagienoah04@gmail.com>
2022-02-01 10:29:46 +0100

    remove Query type on resolver

--- src/app.js
+++ src/app.js
@@ -6,27 +6,25 @@ import { typeDefs } from "./schema.js";
 app.use(morgan("dev"));
 
 const resolvers = {
-  Query: {
-    staff() {
-      return {
-        id: "1345324451",
-        company: "Dangote",
-        employee: [
-          { id: "232341234", name: "Jacky" },
-          { id: "23513421", name: "Michael" },
-        ],
-        departments: [
-          {
-            engineering: {
-              name: "Bob",
-              noofEmployees: 45,
-              employee_Id: ["7883563", "134235", "23561436", "21311235"],
-              manager: 3,
-            },
+  staff() {
+    return {
+      id: "1345324451",
+      company: "Dangote",
+      employee: [
+        { id: "232341234", name: "Jacky" },
+        { id: "23513421", name: "Michael" },
+      ],
+      departments: [
+        {
+          engineering: {
+            name: "Bob",
+            noofEmployees: 45,
+            employee_Id: ["7883563", "134235", "23561436", "21311235"],
+            manager: 3,
           },
-        ],
-      };
-    },
+        },
+      ],
+    };
   },
 };
 
